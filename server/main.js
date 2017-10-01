363869606964e07b51f86cea9bc1f54588b1eb05
Unknown <roiybm@gmail.com>
2017-10-01 14:25:32 +0300

    pack message as json

--- server/main.js
+++ server/main.js
@@ -3,11 +3,12 @@ var fs = require('fs');
 var path = require('path');
 
 http.createServer(function (request, response) {
-    console.log('request ', request.url);
+   // console.log('request ', request.url);
 
-    var filePath = '.' + request.url;
-    if (filePath == './')
-        filePath = './client/index.html';
+    var filePath = '..' + request.url;
+    console.log('filepath = ', filePath);
+    if (filePath == '../')
+        filePath = '../client/index.html';
 
     var extname = String(path.extname(filePath)).toLowerCase();
     var contentType = 'text/html';
